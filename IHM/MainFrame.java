c058ae6ed469b649f7bd96becc9474de750a3ec9
LilBrainMeiz <enzo.loubeau@etu.univ-lehavre.fr>
2021-05-09 23:33:52 +0200

    first commit

---
+++ IHM/MainFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame
+{
+    public MainFrame()
+    {
+        this.setTitle("Mini-projet");
+        this.setSize(600, 600);
+        this.setLocation(20, 20);
+
+        this.setVisible(true);
+    }
+}
