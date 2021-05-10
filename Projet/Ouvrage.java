22014dfda3d31654db0ef198657e894301e3b467
LilBrainMeiz <enzo.loubeau@etu.univ-lehavre.fr>
2021-05-10 11:38:42 +0200

    Ajout de la factory pour la limite des 10000

--- Projet/Ouvrage.java
+++ Projet/Ouvrage.java
@@ -11,22 +11,14 @@ public class Ouvrage
     private String  sDessinateur;
     private Integer iTome; // Ici le tome est en Integer car il est facultatif
 
-    public Ouvrage(String titre, String editeur, String scenariste,
-                   String serie, String dessinateur)
+    public static creerOuvrage(String titre, String editeur, String serie,
+                               String scenariste, String dessinateur,
+                               Integer tome)
     {
-        this(titre, editeur, serie, scenariste, dessinateur, null);
-    }
+        if(Ouvrage.isNombreOuvrage == 10000)return null;
 
-    public Ouvrage(String titre, String editeur, String scenariste,
-                   String dessinateur, Integer tome)
-    {
-        this(titre, editeur, null, scenariste, dessinateur, tome);
-    }
-
-    public Ouvrage(String titre, String editeur, String scenariste,
-                   String dessinateur)
-    {
-        this(titre, editeur, null, scenariste, dessinateur, null);
+        return new Ouvrage(titre, editeur, serie, scenariste, dessinateur,
+                           tome);
     }
 
     private Ouvrage(String titre, String editeur, String serie,
@@ -48,4 +40,10 @@ public class Ouvrage
     public String  getScenariste() { return this.sScenariste;  }
     public String  getTitre()      { return this.sTitre;       }
     public Integer getTome()       { return this.iTome;        }
+
+    public String toString()
+    {
+        return getTitre() + "|" + getScenariste() + "|" + getDessinateur() +
+               "|" + getEditeur() + "|" + getSerie() + "\n";
+    }
 }
