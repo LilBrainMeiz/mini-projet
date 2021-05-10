706278e115bba856a8daa2791fbd2db0d77aa757
nnoma17 <72576115+nnoma17@users.noreply.github.com>
2021-05-10 09:55:55 +0200

    Update ControleurEtat.java

--- Projet/ControleurEtat.java
+++ Projet/ControleurEtat.java
@@ -1,7 +1,15 @@
 public class ControleurEtat
 {
+    private FrameEtat  ihm;
+    private MetierEtat metier;
+
     public ControleurEtat()
     {
-        
+        this.ihm    = new FrameEtat(this);
+        this.metier = new metierEtat();
+    }
+
+    public static public static void main(String[] args) {
+        new ControleurEtat();
     }
 }
