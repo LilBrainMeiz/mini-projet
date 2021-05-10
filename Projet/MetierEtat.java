7086fa7c763a45ec326f235739d219641fe13131
Benjihs2002 <70745884+Benjihs2002@users.noreply.github.com>
2021-05-10 10:48:34 +0200

    Update MetierEtat.java

--- Projet/MetierEtat.java
+++ Projet/MetierEtat.java
@@ -1,7 +1,25 @@
-public class MetierEtat
+import javax.swing.*;
+
+
+public class MetierEtat extends JFrame
 {
+	private ControleurEtat  ctrl;
+
+	private PanelEtat  panelEtat;
+
 	public MetierEtat()
 	{
+		this.setTitle( "Etats" );
+		this.setLocation( 30, 30 );
+		this.setSize( 400, 200 );
+
+		this.panelEtat  = new PanelEtat( this.ctrl );
+
+        this.add( this.panelEtat );
+
+
+		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        this.setVisible( true );
 
 	}
 }
