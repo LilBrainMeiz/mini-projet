47e2889463520763fdfbc9b6e7ca2456d06e111b
LilBrainMeiz <enzo.loubeau@etu.univ-lehavre.fr>
2021-05-10 09:48:55 +0200

    lireFichier

--- Projet/MetierBase.java
+++ Projet/MetierBase.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MetierBase
 {
@@ -8,5 +9,20 @@ public class MetierBase
     public MetierBase()
     {
         this.ensOuvrages = new ArrayList<Ouvrage>();
+    }
+
+    public List<String> lireFichier(String chemin)
+    {
+        List<String> oFichier = new ArrayList<String>();
+
+        Scanner oInput = new Scanner(new File(path));
+
+        // Bug ici si le fichier est vide;
+        do
+        {
+            oFichier.add(oInput.nextLine());
+        }while(oInput.hasNextLine());
+
+        return oFichier;
     }
 }
