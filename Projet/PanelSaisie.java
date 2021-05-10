3b1a4dc50a78890bf3ef80c8dc88ae1f03fe4555
Benjihs2002 <70745884+Benjihs2002@users.noreply.github.com>
2021-05-10 09:53:42 +0200

    Update PanelSaisie.java
    Création des composants

--- Projet/PanelSaisie.java
+++ Projet/PanelSaisie.java
@@ -1,7 +1,37 @@
+import javax.swing.*;
+
 public class PanelSaisie
 {
-    public PanelSaisie()
+    private ControleurSaisie ctrl;
+    
+    private JTextField txtTitre;
+    private JTextField txtEditeur;
+    private JTextField txtDessinateur;
+    private JTextField txtScenariste;
+    private JTextField txtTome;
+    private JTextField txtTome;
+    private JTextField txtSerie;
+    
+    private JButton btnAjouter;
+    
+    public PanelSaisie(ControleurSaisie ctrl)
     {
+        this.ctrl = ctrl;
+        
+        /*-----------------------------*/
+        /*   Création des composants   */
+        /*-----------------------------*/
+        
+        txtTitre       = new JTextField(14);
+        txtEditeur     = new JTextField(14);
+        txtDessinateur = new JTextField(14);
+        txtScenariste  = new JTextField(14);
+        txtTome        = new JTextField(14);
+        txtTome        = new JTextField(14);
+        txtSerie       = new JTextField(14);
+        
+        btnAjouter     = new JButton("Ajouter la BD");
+        
         
     }
 }
