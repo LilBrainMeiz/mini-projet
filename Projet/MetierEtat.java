ab9f4677d03c600d67d6cdae8ef286305d08b086
Cerbultubage99 <76993234+Cerbultubage99@users.noreply.github.com>
2021-05-10 10:40:13 +0200

    Update MetierEtat.java

--- Projet/MetierEtat.java
+++ Projet/MetierEtat.java
@@ -1,7 +1,30 @@
-public class MetierEtat extends MetierBase
+import javax.swing.*;
+import java.awt.BorderLayout;
+
+
+public class FrameEtat extends JFrame
 {
-    public MetierEtat()
-    {
-        
-    }
+	private Controleur  ctrl;
+
+	private PanelEtat  panelEtat;
+
+	public FrameEtat( Controleur  ctrl )
+	{
+
+
+		this.ctrl =  ctrl;
+
+		this.setTitle( "Etats" );
+		this.setLocation( 30, 30 );
+		this.setSize( 400, 200 );
+
+		this.panelEtat  = new PanelEtat( this.ctrl );
+
+        this.add( this.panelEtat );
+
+
+		this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        this.setVisible( true );
+
+	}
 }
