c618bfcbda6ef078c6fea28a15dcf11ee9d6b629
LilBrainMeiz <enzo.loubeau@etu.univ-lehavre.fr>
2021-05-10 10:48:03 +0200

    Fix metierSaisie

--- Projet/MetierSaisie.java
+++ Projet/MetierSaisie.java
@@ -1,33 +1,7 @@
 public class MetierSaisie extends MetierBase
 {
-    private titre;
-    private editeur;
-    private scenariste;
-    private tome;
-    private serie;
-
-    public void setTitre ( String titre )
+    public MetierSaisie()
     {
-        this.titre = titre;
-    }
-
-    public void setEditeur ( String editeur )
-    {
-        this.editeur = editeur;
-    }
-
-    public void setScenariste ( String scenariste )
-    {
-        this.scenariste = scenariste;
-    }
-
-    public void setTome ( String tome)
-    {
-        this.tome = tome;
-    }
-
-    public void setSerie ( String serie )
-    {
-        this.serie = serie;
+        
     }
 }
