3ac14ccd46bd5a44132bd8303c578aaf3f830ee2
MathisPesquerel <83927725+MathisPesquerel@users.noreply.github.com>
2021-05-10 10:56:18 +0200

    Update FrameSaisie.java

--- Projet/FrameSaisie.java
+++ Projet/FrameSaisie.java
@@ -9,19 +9,23 @@ public class FrameSaisie extends JFrame
     {
         // Modèle MVC
         this.oCtrl = ctrl;
-
+        
+        //Caractèristiques de la frame
         this.setTitle("Mini-projet");
         this.setSize(600, 600);
         this.setLocation(20, 20);
         
+        //Création des composants
         this.oPanelSaisie = new PanelSaisie(oCtrl);
         
+        //Ajout des composants
         this.add(this.oPanelSaisie);
         
         this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         this.setVisible(true);
     }
     
+    //Mise à jour du panel
     public void maj()
     {
         this.oPanelSaisie.maj();
