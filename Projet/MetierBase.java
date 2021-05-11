1661908ed6e5b6db2ea29e508298167eda2f0764
Benjihs2002 <70745884+Benjihs2002@users.noreply.github.com>
2021-05-11 10:30:36 +0200

    Update MetierBase.java

--- Projet/MetierBase.java
+++ Projet/MetierBase.java
@@ -2,30 +2,28 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 
 public class MetierBase
 {
-    //Attribut
     private List<Ouvrage> ensOuvrages;
-    
-    //Constructeur
+
     public MetierBase()
     {
         this.ensOuvrages = new ArrayList<Ouvrage>();
     }
 
-    public void ajouterOuvrage(String titre, String editeur, String serie,
+    public boolean ajouterOuvrage(String titre, String editeur, String serie,
                                String scenariste, String dessinateur,
                                Integer tome)
     {
-        this.ensOuvrages.add(Ouvrage.creerOuvrage(titre, editeur, serie,
-                                                  scenariste, dessinateur,
-                                                  tome));
+        Ouvrage tmp = Ouvrage.creerOuvrage(titre, editeur, serie,
+                                           scenariste, dessinateur, tome);
+
+        if ( tmp == null )return false;
+        
+        this.ensOuvrages.add(tmp);
+        return true;
     }
 
     public ArrayList<String> lireFichier(String chemin)
@@ -46,67 +44,7 @@ public class MetierBase
         return oFichier;
     }
 
-    public void ecrireFichier(String chemin, String toPrint)
-    {
-        try
-        {
-            PrintWriter oOutput = new PrintWriter( new FileWriter(chemin, true));
-        
-            oOutput.println(toPrint);
-            oOutput.close();
-        }
-        catch(Exception e){ e.printStackTrace(); }
-    }
+    public List<Ouvrage> getOuvrages   (){ return this.ensOuvrages; }
 
-    public String toString()
-    {
-        File f = new File("sortie.txt");
-        boolean bIsExisting = f.exists();
-
-        if(!bIsExisting)
-        {
-            String sEntete = "+";
-            String suite = "------------------------------+";
-    
-            for(int i = 0; i < 5; i++)
-            {
-                sEntete += suite;
-            }
-    
-            sEntete += "\n";
-
-            String encadre = "|" + String.format("%-30s", "Titre") + "|" +
-            String.format("%-30s", "Scenariste")  + "|" +
-            String.format("%-30s", "Dessinateur") + "|" + 
-            String.format("%-30s", "Editeur")     + "|" +
-            String.format("%-30s", "Serie")       + "|" + "\n";
-
-            sEntete += encadre + "+";
-
-            for(int i = 0; i < 5; i++)
-            {
-                sEntete += suite;
-            }
-
-            sEntete += "\n";
-
-            for(Ouvrage o : this.ensOuvrages)
-            {
-                sEntete += o.toString();
-            }
-
-            return sEntete;
-        }
-        else
-        {
-            String sRet = "";
-
-            for(Ouvrage o : this.ensOuvrages)
-            {
-                sRet += o.toString();
-            }
-
-            return sRet;
-        }
-    }
+    public Ouvrage       getLastOuvrage(){ return this.ensOuvrages.get(this.ensOuvrages.size()-1);}
 }
