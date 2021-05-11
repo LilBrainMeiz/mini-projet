52402290fcc400b6525a3b96e93192dafc744acf
nnoma17 <72576115+nnoma17@users.noreply.github.com>
2021-05-11 12:45:27 +0200

    Update MetierEtat.java

--- Projet/MetierEtat.java
+++ Projet/MetierEtat.java
@@ -11,42 +11,42 @@ import java.io.FileInputStream;
 
 public class MetierEtat extends MetierBase
 {
-    public void rechercheDansFichier( String recherche, String chemin )
-    {
-        System.out.println( super.enTete() );
-        try
-        {
-            Scanner sc = new Scanner( new FileInputStream( chemin ) );
+	public void rechercheDansFichier( String recherche, String chemin )
+	{
+		System.out.println( super.enTete() );
+		try
+		{
+			Scanner sc = new Scanner( new FileInputStream( chemin ) );
 
-            while( sc.hasNextLine() )
-            {
-                String tmp;
-                tmp = sc.nextLine();
-                if( tmp.matches( recherche ) )
-                {
-                    System.out.println( tmp );
-                }
+			while( sc.hasNextLine() )
+			{
+				String tmp;
+				tmp = sc.nextLine();
+				if( tmp.matches( recherche ) )
+				{
+					System.out.println( tmp );
+				}
             }
 
-            sc.close();
-        }
-        catch (Exception e){ e.printStackTrace(); }
-    }
+			sc.close();
+		}
+		catch (Exception e){ e.printStackTrace(); }
+	}
 
-    public void afficheTous()  // a completer
-    {
-        try
-        {
-            Scanner sc = new Scanner( new FileInputStream( "./sortie.txt" ) );
+	public void afficheTous()  // a completer
+	{
+		try
+		{
+			Scanner sc = new Scanner( new FileInputStream( "./sortie.txt" ) );
 
-            while ( sc.hasNextLine() )
-            {
-                tmp = sc.nextLine();
-                System.out.println( tmp );
+			while ( sc.hasNextLine() )
+			{
+				tmp = sc.nextLine();
+				System.out.println( tmp );
 
-            }
-            sc.close();
-        }
-        catch (Exception e){ e.printStackTrace(); }
-    }
+			}	
+			sc.close();
+		}
+		catch (Exception e){ e.printStackTrace(); }
+	}
 }
