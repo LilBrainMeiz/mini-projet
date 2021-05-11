03f4b7629ce9e3aa900f3e6a0128128dd7921d54
Cerbultubage99 <76993234+Cerbultubage99@users.noreply.github.com>
2021-05-11 12:31:45 +0200

    Update EtatCUI.java

--- Projet/EtatCUI.java
+++ Projet/EtatCUI.java
@@ -1,1 +1,20 @@
+import iut.algo.Clavier;
 
+public class EtatCUI
+{
+	private String methodeAppeler;
+
+	public EtatCUI()
+	{
+		System.out.println( "Que souhaitez vous faire ?"                                    + "\n" +
+		                    "   1.Afficher tous les ouvrages"                               + "\n" +
+		                    "   2.Afficher tous les ouvrages triée par Editeurs et Séries." + "\n" +
+		                    "   3.Afficher tous les ouvrages de [nom]"                      + "\n"
+		                  );
+
+		System.out.print( "Saisissez le numero puis son eventuel parametre : " );
+		this.methodeAppeler = Clavier.lireString();
+	}
+
+	public String getMethodeAppeler(){ return this.methodeAppeler; }
+}
