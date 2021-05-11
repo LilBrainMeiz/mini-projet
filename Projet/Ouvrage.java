0bb7ebd01c91dbce17b6b887ee442ceb2bf88783
Benjihs2002 <70745884+Benjihs2002@users.noreply.github.com>
2021-05-11 11:44:11 +0200

    Update Ouvrage.java

--- Projet/Ouvrage.java
+++ Projet/Ouvrage.java
@@ -43,20 +43,41 @@ public class Ouvrage
         this.iTome          = tome;
     }
 
-    public String  getEditeur()    { return this.sEditeur;     }
+        public String  getEditeur    (){ return this.sEditeur;     }
     public String  getDessinateur(){ return this.sDessinateur; }
-    public String  getSerie()      { return this.sSerie;       }
-    public String  getScenariste() { return this.sScenariste;  }
-    public String  getTitre()      { return this.sTitre;       }
-    public Integer getTome()       { return this.iTome;        }
+    public String  getSerie      (){ return this.sSerie;       }
+    public String  getScenariste (){ return this.sScenariste;  }
+    public String  getTitre      (){ return this.sTitre;       }
+    public Integer getTome       (){ return this.iTome;        }
 
     public String toString()
     {
-        return "|" + String.format("%-30s", this.getTitre())       + "|" +
-                     String.format("%-30s", this.getScenariste())  + "|" +
-                     String.format("%-30s", this.getDessinateur()) + "|" + 
-                     String.format("%-30s", this.getEditeur())     + "|" +
-                     String.format("%-30s", this.getSerie())       + "|" + 
-                     String.format("%-30s", this.getTome())        + "|" + "\n";
+        return "| " + String.format("%-30s", this.getTitre      ()) + "| " +
+                      String.format("%-30s", this.getScenariste ()) + "| " +
+                      String.format("%-30s", this.getDessinateur()) + "| " + 
+                      String.format("%-30s", this.getEditeur    ()) + "| " +
+                      String.format("%-30s", this.getSerie      ()) + "| " + 
+                      String.format("%-30s", this.getTome       ()) + "|";
+    }
+
+    public int compareTo(Ouvrage autre)
+    {
+        int iSize = autre.sTitre.length();
+        int iVal;
+
+        if ( this.sTitre.length() < iSize )iSize = this.sTitre.length();
+
+        for(int i=0;i<iSize;i++)
+        {
+            iVal = this.sTitre.charAt(i) - autre.sTitre.charAt(i);
+            if ( iVal != 0 )return iVal; 
+        }
+
+        return 0;
+    }
+
+    public boolean equals(Ouvrage autre)
+    {
+        return this.toString().equals(autre.toString());
     }
 }
