b7434063eb0f0b1f39ad06ad379eae0bffd48194
MathisPesquerel <83927725+MathisPesquerel@users.noreply.github.com>
2021-05-11 16:56:15 +0200

    Create ControleurEtat.java

--- Projet/ControleurEtat.java
+++ Projet/ControleurEtat.java
@@ -1,25 +1,50 @@
+/*
+ * ControleurEtat.java
+ * controleur
+ * @author Bosquain  Maxence
+ * @author Cléon     Benjamin
+ * @author Loubeau   Enzo
+ * @author Pesquerel Mathis
+ * @author Vatres    Manon
+ */
 import java.util.Scanner;
 import java.util.List;
 
 public class ControleurEtat
 {
+	// Attributs
 	private IhmCUI     ihm;
 	private MetierEtat metier;
-    private Scanner    scInput;
-
+    	private Scanner    scInput;
+	
+	// Constructeur
 	public ControleurEtat()
 	{
 		this.ihm    = new IhmCUI(this);
 		this.metier = new MetierEtat();
 
-        scInput = new Scanner(System.in);
+        	scInput = new Scanner(System.in);
 
 		this.lancerEtat();
 	}
-
+	
+	// Accesseurs
+	
+	/*
+	 * appelle enTete dans la classe metier
+	 * @return l'en-tête dans metier
+	 */
 	public String        getEnTete  (){ return this.metier.enTete     (); }
+	
+	/*
+	 * appelle getOuvrages dans la classe metier
+	 * @return l'ensembles des ouvrages dans la classe metier
+	 */
 	public List<Ouvrage> getOuvrages(){ return this.metier.getOuvrages(); }
-
+	
+	/*
+	 * lance l'état
+	 */
 	private void lancerEtat()
 	{
 		int action;
@@ -34,7 +59,7 @@ public class ControleurEtat
 			switch( action )
 			{
 				case 1  -> this.ihm.afficherTriNaturel();
-				case 2  -> this.ihm.afficherListeGroupee();
+				case 2  -> this.ihm.afficherTriNaturel();
 				case 3  -> this.ihm.afficherTriNaturel();
 			}			
 		}while ( action != 4 );
