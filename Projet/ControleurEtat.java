091b0b02f548321a5244395bd3bc6b12a46c90b9
Benjihs2002 <70745884+Benjihs2002@users.noreply.github.com>
2021-05-11 12:32:26 +0200

    Update ControleurEtat.java

--- Projet/ControleurEtat.java
+++ Projet/ControleurEtat.java
@@ -1,14 +1,14 @@
 public class ControleurEtat
 {
-    private MetierEtat metier;
+	private MetierEtat metier;
 
-    public ControleurEtat()
-    {
-        this.metier = new MetierEtat();
-    }
+	public ControleurEtat()
+	{
+		this.metier = new MetierEtat();
+	}
 
-    public static void main(String[] args)
-    {
-        new ControleurEtat();
-    }
+	public static void main(String[] args)
+	{
+		new ControleurEtat();
+	}
 }
