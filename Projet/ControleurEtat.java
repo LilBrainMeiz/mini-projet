00491bae86354e3a827814790193a50cb1348336
Cerbultubage99 <76993234+Cerbultubage99@users.noreply.github.com>
2021-05-11 12:34:16 +0200

    Update ControleurEtat.java

--- Projet/ControleurEtat.java
+++ Projet/ControleurEtat.java
@@ -1,10 +1,30 @@
 public class ControleurEtat
 {
+	private EtatCUI  ihm;
 	private MetierEtat metier;
 
 	public ControleurEtat()
 	{
+		this.ihm    = new EtatCUI();
 		this.metier = new MetierEtat();
+
+		System.out.println( ihm.getMethodeAppeler() );
+		System.out.println( ihm.getMethodeAppeler().charAt(0) );
+		switch( ihm.getMethodeAppeler().charAt(0) + "" )
+		{
+			case "1"  -> this.metier.afficheTous();
+			case "2"  -> System.out.println("truc dur");
+			case "3"  -> {
+							String[] coupe;
+							coupe = new String[2];
+
+							String tmp = ihm.getMethodeAppeler();
+
+							coupe = ihm.getMethodeAppeler().split( " " );
+
+			            	this.metier.rechercheDansFichier( "\\|.*\\|.*"+coupe[1]+".*", "./sortie.txt" );
+			             }
+		}
 	}
 
 	public static void main(String[] args)
